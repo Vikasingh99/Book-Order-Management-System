2a8800954771b0857b6c9683915dd040b7e4b64a
Priyabrata Shee <121084828+Datarush26@users.noreply.github.com>
2026-09-04 00:45:08 +0530

    Delete src directory

--- src/services/axiosPublic.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosPublic = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000',
-  headers: { 'Content-Type': 'application/json' },
-});
-
-export default axiosPublic;
--- src/services/paymentService.js
+++
@@ -1,6 +0,0 @@
-import axiosSecure from './axiosSecure';
-
-export async function processPayment(orderId, paymentMethod) {
-  const res = await axiosSecure.post('/api/payments', { orderId, paymentMethod });
-  return res.data;
-}
